ee84bd2267c8a60fe83160330ea8eac25f070300
nari37 <nareshpogiri409@gmail.com>
2024-09-17 15:27:00 +0530

    "firebase"

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,27 +1,10 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from 'firebase/app';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: 'mern-estate.firebaseapp.com',
-//   projectId: 'mern-estate',
-//   storageBucket: 'mern-estate.appspot.com',
-//   messagingSenderId: '1078482850952',
-//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
-
 
 
 
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -36,4 +19,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export   const app = initializeApp(firebaseConfig);
+   const g_app = initializeApp(firebaseConfig);
+   export const app = getAuth (g_app)
